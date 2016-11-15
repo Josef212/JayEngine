6944a0658008599006c336785673e74f950a591f
Josef21296 <jcasanovasv@hotmail.com>
2016-11-15 20:33:56 +0100

    Resource mesh info and load to vram(opengl)

--- JayEngine/Jay_Engine/ResourceMesh.cpp
+++ JayEngine/Jay_Engine/ResourceMesh.cpp
@@ -1,8 +1,8 @@
 #include "ResourceMesh.h"
 
+#include "OpenGL.h"
 
-
-ResourceMesh::ResourceMesh(uint UID) : Resource(UID)
+ResourceMesh::ResourceMesh(UID uuid) : Resource(uuid)
 {
 	resType = RESOURCE_MESH;
 }
@@ -10,4 +10,75 @@ ResourceMesh::ResourceMesh(uint UID) : Resource(UID)
 
 ResourceMesh::~ResourceMesh()
 {
+	clearResMesh();
+}
+
+bool ResourceMesh::loadToMemory()
+{
+	bool ret = false;
+
+	_LOG(LOG_STD, "Loading mesh to VRAM");
+
+	if (numVertices > 0 && numIndices > 0)
+	{
+		glGenBuffers(1, (GLuint*)&idVertices);
+		glGenBuffers(1, (GLuint*)&idIndices);
+
+		glBindBuffer(GL_ARRAY_BUFFER, idVertices);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numVertices * 3, vertices, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idIndices);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * numIndices, indices, GL_STATIC_DRAW);
+
+		if (numNormals > 0)
+		{
+			glGenBuffers(1, (GLuint*)&idNormals);
+			glBindBuffer(GL_ARRAY_BUFFER, idNormals);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numNormals * 3, normals, GL_STATIC_DRAW);
+		}
+
+		if (numTexCoords> 0)
+		{
+			glGenBuffers(1, (GLuint*)&idTexCoords);
+			glBindBuffer(GL_ARRAY_BUFFER, idTexCoords);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numTexCoords * 2, texCoords, GL_STATIC_DRAW);
+		}
+
+		ret = true;
+	}
+	else
+	{
+		_LOG(LOG_ERROR, "Could not load to VRAM because there are no vertices or indices. Vertices number: %d, indices number: %d", numVertices, numIndices);
+		ret = false;
+	}
+
+	++instancesInMemory;
+
+	return ret;
+}
+
+void ResourceMesh::clearResMesh()
+{
+	if(isInMemory())
+		removeFromMemory();
+
+	numIndices = 0;
+	numVertices = 0;
+	numNormals = 0;
+	numTexCoords = 0;
+
+	RELEASE_ARRAY(indices);
+	RELEASE_ARRAY(vertices);
+	RELEASE_ARRAY(normals);
+	RELEASE_ARRAY(texCoords);
+}
+
+bool ResourceMesh::removeFromMemory()
+{
+	if (idIndices > 0) glDeleteBuffers(1, &idIndices);
+	if (idVertices > 0) glDeleteBuffers(1, &idVertices);
+	if (idNormals > 0) glDeleteBuffers(1, &idNormals);
+	if (idTexCoords > 0) glDeleteBuffers(1, &idTexCoords);
+
+	return true;
 }
--- JayEngine/Jay_Engine/ResourceMesh.h
+++ JayEngine/Jay_Engine/ResourceMesh.h
@@ -8,26 +8,34 @@ class ResourceMesh : public Resource
 public:
 	ResourceMesh(uint UID);
 	virtual ~ResourceMesh();
+
+	bool loadToMemory();
+	bool removeFromMemory();
+
+private:
+	void clearResMesh();
+
+public:
+	uint idVertices = 0;
+	uint numVertices = 0;
+	uint* indices = NULL;
+
+	uint idIndices = 0;
+	uint numIndices = 0;
+	float* vertices = NULL; //!NOTE: remember, the size of the buffer must be the number of vertices * 3
+
+	uint idNormals = 0;
+	uint numNormals = 0;
+	float* normals = NULL; //!NOTE: remember, the size of the buffer must be the number of vertices * 3
+
+	uint idTexCoords = 0;
+	uint numTexCoords = 0;
+	float* texCoords = NULL; //!NOTE: remember, the size of the buffer must be the number of vertices * 2
+
+	//TODO: add vertex colors
+
+private:
+
 };
 
 #endif // !__RESOURCEMESH_H__
-
-
-/*struct VertexInfo
-{
-uint idVertices = 0;
-uint numVertices = 0;
-uint* indices = NULL;
-
-uint idIndices = 0;
-uint numIndices = 0;
-float* vertices = NULL;
-
-uint idNormals = 0;
-uint numNormals = 0; //Each vertex has a normal vec and this var is the size of normals vec(numVertices*3)
-float* normals = NULL;
-
-uint idTexCoords = 0;
-uint numTexCoords = 0;
-float* texCoords = NULL;
-};*/
