855092884dd0e0e5a96b541905af2c14199b5367
Josef21296 <jcasanovasv@hotmail.com>
2017-02-19 13:58:27 +0100

    Checkers loading

--- JayEngine/Jay_Engine/ImporterTexture.cpp
+++ JayEngine/Jay_Engine/ImporterTexture.cpp
@@ -194,9 +194,54 @@ bool ImporterTexture::LoadResource(Resource* resource)
 	return ret;
 }
 
+#define CHECKERS_WIDTH 64
+#define CHECKERS_HEIGHT 64
+
 bool ImporterTexture::LoadChequers(ResourceTexture* res)
 {
 	bool ret = false;
+
+	if (!res)
+		return ret;
+
+	res->originalFile = "-Checkers texture-";
+	res->exportedFile = "-Checkers texture-";
+
+	GLubyte checkImage[CHECKERS_WIDTH][CHECKERS_HEIGHT][4];
+	for (int i = 0; i < CHECKERS_HEIGHT; ++i)
+	{
+		for (int j = 0; j < CHECKERS_WIDTH; ++j)
+		{
+			int c = ((((i & 0x8) == 0) ^ (((j & 0x8)) == 0))) * 255;
+			checkImage[i][j][0] = (GLubyte)c;
+			checkImage[i][j][1] = (GLubyte)c;
+			checkImage[i][j][2] = (GLubyte)c;
+			checkImage[i][j][3] = (GLubyte)255;
+		}
+	}
+
+	uint ImageName = 0;
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	glGenTextures(1, &ImageName);
+	glBindTexture(GL_TEXTURE_2D, ImageName);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, CHECKERS_WIDTH, CHECKERS_HEIGHT,
+		0, GL_RGBA, GL_UNSIGNED_BYTE, checkImage);
+
+	res->width = CHECKERS_WIDTH;
+	res->height = CHECKERS_HEIGHT;
+	res->bpp = 1;
+	res->depth = 4;
+	res->mips = 0;
+	res->bytes = sizeof(GLubyte) * CHECKERS_HEIGHT * CHECKERS_WIDTH * 4;
+	res->format = ResourceTexture::RGBA;
+	res->textureGlID = ImageName;
 
 	return ret;
 }
